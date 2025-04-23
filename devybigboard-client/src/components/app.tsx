be6d696254487b3b3cfac60c680c122a5a980a63
Braden Borman <bradenborman@hotmail.com>
2025-04-23 14:58:33 -0500

    forwards to https

--- devybigboard-client/src/components/app.tsx
+++ devybigboard-client/src/components/app.tsx
@@ -3,6 +3,14 @@ import './app.scss';
 import AppWrapper from './appWrapper';
 
 const App: React.FC = () => {
+  if (
+    window.location.protocol === 'http:' &&
+    window.location.hostname !== 'localhost'
+  ) {
+    window.location.href = window.location.href.replace('http:', 'https:');
+    return null;
+  }
+
   return (
     <div className="app">
       <AppWrapper />
--- devybigboard-client/src/components/appWrapper.tsx
+++ devybigboard-client/src/components/appWrapper.tsx
@@ -4,6 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import ConsentBanner from './ConsentBanner';
 
 const AppWrapper: React.FC = () => {
+    // Redirect to HTTPS if using HTTP in production
+    if (
+        window.location.protocol === 'http:' &&
+        window.location.hostname !== 'localhost'
+    ) {
+        window.location.href = window.location.href.replace('http:', 'https:');
+        return null; // prevent rendering during redirect
+    }
 
     return (
         <BrowserRouter>
