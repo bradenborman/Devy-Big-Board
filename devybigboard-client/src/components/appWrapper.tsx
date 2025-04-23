0b73037189a98654671a67b03122e1f9b3efdb42
Braden Borman <bradenborman@hotmail.com>
2025-04-23 14:08:24 -0500

    added google analyitcs

--- devybigboard-client/src/components/appWrapper.tsx
+++ devybigboard-client/src/components/appWrapper.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MainComponent from './mainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+import usePageTracking from '../usePageTracking';
 
 const AppWrapper: React.FC = () => {
+    useEffect(() => {
+        ReactGA.initialize('G-03DV8L1WEX');
+    }, []);
+
+    usePageTracking();
+
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
