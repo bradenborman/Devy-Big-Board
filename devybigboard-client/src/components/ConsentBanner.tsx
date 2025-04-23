3875ce34264acfccb47399242e8e2dcb75d2864a
Braden Borman <bradenborman@hotmail.com>
2025-04-23 14:33:52 -0500

    idk anymore

--- devybigboard-client/src/components/ConsentBanner.tsx
+++ devybigboard-client/src/components/ConsentBanner.tsx
@@ -6,16 +6,44 @@ const ConsentBanner: React.FC = () => {
 
     useEffect(() => {
         const consent = localStorage.getItem('cookie_consent');
-        if (!consent) setVisible(true);
+
+        if (consent === 'granted') {
+            window.gtag?.('consent', 'update', {
+                ad_storage: 'granted',
+                analytics_storage: 'granted',
+            });
+            window.gtag?.('config', 'G-03DV8L1WEX');
+        } else if (consent === 'denied') {
+            window.gtag?.('consent', 'update', {
+                ad_storage: 'denied',
+                analytics_storage: 'denied',
+            });
+        } else {
+            setVisible(true);
+        }
     }, []);
 
     const acceptCookies = () => {
         localStorage.setItem('cookie_consent', 'granted');
+
+        window.gtag?.('consent', 'update', {
+            ad_storage: 'granted',
+            analytics_storage: 'granted',
+        });
+
+        window.gtag?.('config', 'G-03DV8L1WEX');
+
         setVisible(false);
     };
 
     const declineCookies = () => {
         localStorage.setItem('cookie_consent', 'denied');
+
+        window.gtag?.('consent', 'update', {
+            ad_storage: 'denied',
+            analytics_storage: 'denied',
+        });
+
         setVisible(false);
     };
 
@@ -23,7 +51,7 @@ const ConsentBanner: React.FC = () => {
 
     return (
         <div className="consent-banner">
-            <p>This site uses cookies to enhance the user experience.</p>
+            <p>This site uses cookies to improve your experience. Do you accept analytics tracking?</p>
             <div className="buttons">
                 <button onClick={acceptCookies}>Accept</button>
                 <button onClick={declineCookies}>Decline</button>
--- devybigboard-client/src/components/appWrapper.tsx
+++ devybigboard-client/src/components/appWrapper.tsx
@@ -1,29 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import MainComponent from './mainComponent';
 import { BrowserRouter } from 'react-router-dom';
-import ReactGA from 'react-ga4';
-import usePageTracking from '../usePageTracking';
 import ConsentBanner from './ConsentBanner';
 
 const AppWrapper: React.FC = () => {
-    useEffect(() => {
-        ReactGA.initialize('G-03DV8L1WEX');
-    }, []);
 
     return (
         <BrowserRouter>
-            <PageTracker />
             <ConsentBanner />
             <div className='app-wrapper'>
                 <MainComponent />
             </div>
         </BrowserRouter>
     );
-};
-
-const PageTracker: React.FC = () => {
-    usePageTracking();
-    return null;
 };
 
 export default AppWrapper;
